a3a2a296ebfd618c3d7ff8f7893ce9d79a6d939d
nisan250 <nisan250@gmail.com>
2020-02-11 20:17:08 +0000

    adding theme and small configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,31 @@
 import { addParameters, configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 import '@storybook/addon-console';
+
+
+//NISAN: "global style" example
+// import "../bootstrap-reboot.min.css";
+// import "../bootstrap.css";
+// import "../bootstrap-grid.css";
+// import "../main.css";
+
+	
+// addParameters({
+// 	options: {
+// 		theme: crfTheme
+// 	//	theme: nisanTheme
+// 	}
+// });
+
+addDecorator(withInfo({
+	inline: true
+}));
+
+addDecorator(withKnobs);
+
+function loadStories(){
+	const req= require.context('../stories', true, /\.stories\.js$/);
+	req .keys().forEach(filename => req(filename));
+}
+configure(loadStories, module);
---
+++ .storybook/myTheme.js
@@ -0,0 +1,40 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  BrandTitle: "Nisan Sabag Storybook",
+  // brandImage: "../Images/nisan-logo.jpeg",
+  brandImage: "https://nisan-sabag.com/wp-content/themes/nisan-theme/assets/images/nisan-sabag.png",
+  colorSecondary: "#ea1919"
+  // colorPrimary: 'hotpink',
+  // colorSecondary: 'deepskyblue',
+
+  // // UI
+  // appBg: 'white',
+  // appContentBg: 'silver',
+  // appBorderColor: 'grey',
+  // appBorderRadius: 4,
+
+  // // Typography
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: 'monospace',
+
+  // // Text colors
+  // textColor: 'black',
+  // textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // // Toolbar default and active colors
+  // barTextColor: 'silver',
+  // barSelectedColor: 'black',
+  // barBg: 'hotpink',
+
+  // // Form colors
+  // inputBg: 'white',
+  // inputBorder: 'silver',
+  // inputTextColor: 'black',
+  // inputBorderRadius: 4,
+
+  // brandTitle: 'My custom storybook',
+  // brandUrl: 'https://example.com',
+  // brandImage: 'https://placehold.it/350x150',
+});
